c259a8991b75e85a49c5a284314875d3b84dd4e7
Kishore.M <kishoresabari007@gmail.com>
2025-04-18 22:21:48 +0530

    add vowels and consonants in string.java

---
+++ add vowels and consonants in string.java
@@ -0,0 +1,19 @@
+public class demo3 {
+    public static void main(String[] args) {
+        String str="Hello World";
+        int vowels=0;
+        int consonants=0;
+        for(char c:str.toLowerCase().toCharArray()){
+            if(c>='a' && c<='z'){
+                if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+                    vowels++;
+                }else{
+                    consonants++;
+                }
+            }
+        }
+        System.out.println("Vowels:"+vowels);
+        System.out.println("Consonants:"+consonants);
+    }
+    
+}
