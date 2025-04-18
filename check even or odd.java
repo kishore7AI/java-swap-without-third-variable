e3a27e392e3faae1da3e72faefdfce86b1052340
Kishore.M <kishoresabari007@gmail.com>
2025-04-18 22:20:34 +0530

    checkevenorodd.java

---
+++ check even or odd.java
@@ -0,0 +1,11 @@
+public class demo2 {
+    public static void main(String[] args) {
+        int a=10;
+        if((a&1)==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
+    
+}
